7792486ce0f10c6d2c2afe177071e1f9f75fb2da
Beamer <beamer@working.studio>
2024-03-20 10:56:38 -0700

    Re-paste boilerplate

--- app/plasmic-init.ts
+++ app/plasmic-init.ts
@@ -1,12 +1,13 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs/react-server-conditional"
-
 export const PLASMIC = initPlasmicLoader({
-  preview: process.env.NEXT_PUBLIC_PLASMIC_PREVIEW === "true",
   projects: [
     {
-      id: process.env.NEXT_PUBLIC_PLASMIC_PROJECT ?? "",
-      token: process.env.NEXT_PUBLIC_PLASMIC_TOKEN ?? "",
-      version: process.env.PLASMIC_VERSION,
+      id: "o18PCvr4rF9aSHNWHVzuzb", // ID of a project you are using
+      token:
+        "oemFKB0YK8R1e09XPlcjGH47sfRgDCFsFSnAij6rA3QsbPcD77n8Ayf3FTOFx0fNF3XVtln5v1yYKQVBWtA", // API token for that project
     },
   ],
+  // Fetches the latest revisions, whether or not they were unpublished!
+  // Disable for production to ensure you render only published changes.
+  preview: true,
 })
